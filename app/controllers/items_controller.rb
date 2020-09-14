66c7099e04632db17797b1ee16b3216680faac18
tomocanan <happyflower1914@gmail.com>
2020-09-14 13:06:14 +0900

    ユーザー管理機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,11 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  
-  def index
-    @item = Item.all
-  end
+
+  # def index
+  #   @item = Item.all
+  # end
 
   def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless user_signed_in?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/ }
   validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   has_many :items
 end
