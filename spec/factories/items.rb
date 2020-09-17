880fc1b2ce24afe9207888c6ae6672ba324a6b9d
tomocanan <happyflower1914@gmail.com>
2020-09-17 20:06:44 +0900

    商品出品機能修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -7,7 +7,7 @@ FactoryBot.define do
     delivery_fee_id    { 2 }
     prefecture_id      { 2 }
     day_id             { 2 }
-    price              { Faker::Number.between(from: 201, to: 9_999_999) }
+    price              { Faker::Number.between(from: 300, to: 9_999_999) }
 
     association :user
 
