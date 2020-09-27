a0782b61338430f659c6ebd04a97d26ff10a3405
tomocanan <happyflower1914@gmail.com>
2020-09-27 21:17:01 +0900

    バリデーション修正

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,10 +1,10 @@
 class Buyer < ApplicationRecord
-  validates :area, presence: true
-  validates :city, presence: true
-  validates :address1, presence: true
-  
-  validates :tell, presence: true
-  validates :postal, presence: true
-  
+  with_options presence: true do
+    validates :area
+    validates :city
+    validates :address1
+    validates :tell, format: { with: /(0{1}\d{9,10})/ }
+    validates :postal, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  end
   belongs_to :order
 end
