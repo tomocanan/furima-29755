63e1938639dbf2dcd46798a4f06e3307f653a478
tomocanan <happyflower1914@gmail.com>
2020-09-28 20:45:43 +0900

    不要な改行削除

--- spec/factories/order_buyers.rb
+++ spec/factories/order_buyers.rb
@@ -1,11 +1,5 @@
 FactoryBot.define do
   factory :order_buyer do
-    # number           { "4242424242424242" }
-    # cvc              { Faker::Number.number(digits: 3) }
-    # exp_month        { Faker::Number.between(from: 1, to: 12) }
-    # exp_year         { "21" }
-
-    # token            { "0x00007f9d7b3a0078" }
     postal           { '000-0000' }
     area             { 2 }
     city             { '札幌市' }
